fea590924fdf86dd8f51da933070acdc323bcc89
justus <henze5@gmx.de>
2024-04-16 21:06:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,15 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 21.0.2-tem
 apt install mysql-server
 PASSWORD=$('keyword' | sha1sum)
+mkdir /etc/config
 echo "spring.datasource.password = " > /etc/config/kjgtermine.properties
 $PASSWORD >> /etc/config/kjgtermine.properties
-mysql -u root
-CREATE DATABASE kjgtermine;
-CREATE USER 'kjgtermine'@'localhost' IDENTIFIED BY "$PASSWORD";
-GRANT ALL ON *.* TO 'kjgtermine'@'localhost';
-exit
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/kjgtermine.jar
+# shellcheck disable=SC2027
+mysql -u root --execute="CREATE DATABASE kjgtermine; CREATE USER 'kjgtermine'@'localhost' IDENTIFIED BY ""$PASSWORD"";GRANT ALL ON *.* TO 'kjgtermine'@'localhost';"
+curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/download/kjgtermine.jar
 useradd kjgtermine
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/kjgtermine.service
+curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/download/kjgtermine.service
 mv kjgtermine.service /etc/systemd/system/
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/update.sh
+curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/download/update.sh
 chmod +x update.sh
 systemctl start kjgtermine
