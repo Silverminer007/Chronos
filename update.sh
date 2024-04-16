9b4d3c2bf369968a2ac134df55f41e85d33935e0
justus <henze5@gmx.de>
2024-04-16 23:01:08 +0200

    Update install script

--- update.sh
+++ update.sh
@@ -10,5 +10,6 @@ PASSWORD=$(echo 'keyword' | sha1sum)
 echo "$PASSWORD"
 mkdir /etc/config
 echo "spring.datasource.password = ""$PASSWORD" > /etc/config/kjgtermine.properties
-mysql -u root --execute="ALTER USER 'kjgtermine'@'localhost' IDENTIFIED BY '""$PASSWORD""';"
+#mysql -u root --execute="ALTER USER 'kjgtermine'@'localhost' IDENTIFIED BY '""$PASSWORD""';"
+mysql -u root --execute="DROP USER 'kjgtermine'@'localhost'; FLUSH PRIVILEGES; CREATE USER 'kjgtermine'@'localhost' IDENTIFIED BY '""$PASSWORD""';GRANT ALL ON *.* TO 'kjgtermine'@'localhost';"
 systemctl start kjgtermine
