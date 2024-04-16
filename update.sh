5a8e997ec0eba4ea45aef78fd711f6016de95c32
justus <henze5@gmx.de>
2024-04-16 22:56:25 +0200

    Update update script

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 systemctl stop kjgtermine
 GITHUB_PAT=$(<../../github.pat)
-git fetch "https://""$GITHUB_PAT""@github.com/Silverminer007/Gruppentool.git"
-cd Gruppentool || echo "Update failed" | exit
+git stash
+git pull "https://""$GITHUB_PAT""@github.com/Silverminer007/Gruppentool.git"
 mvn clean package -Pproduction
 cp kjgtermine.service /etc/systemd/system/
 systemctl daemon-reload
@@ -9,7 +9,6 @@ mv target/kjgtermine-ALPHA.jar ../../kjgtermine.jar
 PASSWORD=$(echo 'keyword' | sha1sum)
 echo "$PASSWORD"
 mkdir /etc/config
-echo "spring.datasource.password = " > /etc/config/kjgtermine.properties
-echo "$PASSWORD" >> /etc/config/kjgtermine.properties
+echo "spring.datasource.password = ""$PASSWORD" > /etc/config/kjgtermine.properties
 mysql -u root --execute="ALTER USER 'kjgtermine'@'localhost' IDENTIFIED BY '""$PASSWORD""';"
 systemctl start kjgtermine
