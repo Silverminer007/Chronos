b075cea9333bc21bdf46316601831e0bea20735a
justus <henze5@gmx.de>
2024-04-16 22:30:19 +0200

    Alter Installation process

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-mkdir /opt/kjgtermine
-cd /opt/kjgtermine || echo "Installation failed" | exit
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 21.0.2-tem
+curl -OL https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
+apt install ./mysql-apt-config_0.8.29-1_all.deb
 apt install mysql-server
 PASSWORD=$('keyword' | sha1sum)
 mkdir /etc/config
@@ -10,10 +10,3 @@ echo "spring.datasource.password = " > /etc/config/kjgtermine.properties
 $PASSWORD >> /etc/config/kjgtermine.properties
 # shellcheck disable=SC2027
 mysql -u root --execute="CREATE DATABASE kjgtermine; CREATE USER 'kjgtermine'@'localhost' IDENTIFIED BY ""$PASSWORD"";GRANT ALL ON *.* TO 'kjgtermine'@'localhost';"
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/download/kjgtermine.jar
-useradd kjgtermine
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/download/kjgtermine.service
-mv kjgtermine.service /etc/systemd/system/
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/download/update.sh
-chmod +x update.sh
-systemctl start kjgtermine
--- update.sh
+++ update.sh
@@ -1,8 +1,15 @@
 systemctl stop kjgtermine
-sdk install java 21.0.2-tem
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/kjgtermine.jar
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/kjgtermine.service
-mv kjgtermine.service /etc/systemd/system/
-curl -OL https://github.com/Silverminer007/Gruppentool/releases/latest/update.sh
-chmod +x update.sh
+GITHUB_PAT=$(<github.pat)
+cd ..
+git clone https://"$GITHUB_PAT"@github.com/Silverminer007/Gruppentool.git
+cd Gruppentool || echo "Update failed" | exit
+mvn clean package -Pproduction
+cp kjgtermine.service /etc/systemd/system/
+systemctl deamon-reload
+mv target/kjgtermine-ALPHA.jar ../../kjgtermine.jar
+PASSWORD=$('keyword' | sha1sum)
+mkdir /etc/config
+echo "spring.datasource.password = " > /etc/config/kjgtermine.properties
+$PASSWORD >> /etc/config/kjgtermine.properties
+mysql -u root --execute="ALTER USER 'kjgtermine'@'localhost' IDENTIFIED BY ""$PASSWORD"";"
 systemctl start kjgtermine
