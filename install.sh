93c8f90702ada019954454d4f248326025d57440
justus <henze5@gmx.de>
2024-04-17 16:37:35 +0200

    Write log files

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sdk install java 21.0.2-tem
 curl -OL https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
 apt install ./mysql-apt-config_0.8.29-1_all.deb
 apt install mysql-server
+mkdir /opt/kjgtermine/logs
 PASSWORD=$(echo 'keyword' | sha1sum)
 mkdir /etc/config
 echo "spring.datasource.password = ""$PASSWORD" > /etc/config/kjgtermine.properties
