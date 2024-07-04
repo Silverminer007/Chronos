8bc15ca82fa81a61f6e8624e99f3cfaefb1dbd1b
justus <henze5@gmx.de>
2024-07-04 22:28:32 +0200

    change way of installation

---
+++ install/update-frontend.sh
@@ -0,0 +1,30 @@
+TOKEN=$3
+REPO="Silverminer007/Chronos"
+FILE=chronos.jar      # the name of your release asset file, e.g. build.tar.gz
+VERSION=$1                       # tag name or the word "latest"
+GITHUB="https://api.github.com"
+
+alias errcho='>&2 echo'
+
+function gh_curl {
+  curl -H "Authorization: token $TOKEN" \
+       -H "Accept: application/vnd.github.v3.raw" \
+       "$@"
+}
+
+if [ "$VERSION" = "latest" ]; then
+  # Github should return the latest release first.
+  parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
+else
+  parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
+fi;
+
+asset_id=$(gh_curl -s $GITHUB/repos/$REPO/releases | jq "$parser")
+if [ "$asset_id" = "null" ]; then
+  echo "ERROR: version not found $VERSION"
+  exit 1
+fi;
+
+wget -q --auth-no-challenge --header='Accept:application/octet-stream' \
+  https://"$TOKEN":@api.github.com/repos/$REPO/releases/assets/"$asset_id" \
+  -O "$2"
