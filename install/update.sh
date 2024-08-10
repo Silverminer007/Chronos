4454c86621d6eaa039df378b48d78226fc4ddac7
justus <justushenze@gmx.de>
2024-08-10 13:47:53 +0200

    Merge Maven Multi Module into main branch

--- install/update.sh
+++ install/update.sh
@@ -1,11 +1,43 @@
+REPO="Silverminer007/Chronos"
+VERSION=latest
+GITHUB="https://api.github.com"
+
+alias errcho='>&2 echo'
+
+function gh_curl {
+  curl -H "Authorization: token $TOKEN" \
+       -H "Accept: application/vnd.github.v3.raw" \
+       "$@"
+}
+
+function downloadAssets {
+    FILE=$1
+    if [ "$VERSION" = "latest" ]; then
+      # Github should return the latest release first.
+      parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
+    else
+      parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
+    fi;
+
+    asset_id=$(gh_curl -s $GITHUB/repos/$REPO/releases | jq "$parser")
+    if [ "$asset_id" = "null" ]; then
+      echo "ERROR: version not found $VERSION"
+      exit 1
+    fi;
+
+    wget -q --auth-no-challenge --header='Accept:application/octet-stream' \
+      https://"$TOKEN":@api.github.com/repos/$REPO/releases/assets/"$asset_id" \
+      -O "$2"
+}
+
 cd /opt/chronos || (echo "Updating Chronos failed" & exit)
 echo Please enter your GitHub PAT
-read -r pat
+read -r TOKEN
 echo "Update Frontend START"
 systemctl stop chronos
-./update-frontend.sh "latest" chronos.jar "$pat"
+downloadAssets chronos-frontend.jar chronos.jar
 systemctl stop chronos
 echo "Update Frontend END"
 echo "Update Cronjob START"
-./update-cron.sh "latest" chronos-cron.jar "$pat"
+downloadAssets chronos-cron.jar chronos-cron.jar
 echo "Update Cronjob END"
--- src/main/java/de/kjgstbarbara/services/GameRepository.java
+++
@@ -1,8 +0,0 @@
-package de.kjgstbarbara.services;
-
-import de.kjgstbarbara.data.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface GameRepository extends JpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
-}
