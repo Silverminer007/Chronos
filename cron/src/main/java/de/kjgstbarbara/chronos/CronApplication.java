a0077af26144e76398276a1977dfaa3e8870b84e
justus <justushenze@gmx.de>
2024-08-11 13:27:59 +0200

    Bring Application main Classes back

---
+++ cron/src/main/java/de/kjgstbarbara/chronos/CronApplication.java
@@ -0,0 +1,12 @@
+package de.kjgstbarbara.chronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronApplication.class, args);
+    }
+}
---
+++ frontend/src/main/java/de/kjgstbarbara/chronos/FrontendApplication.java
@@ -0,0 +1,38 @@
+package de.kjgstbarbara.chronos;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@PWA(
+        name = "Chronos",
+        shortName = "Chronos",
+        offlinePath="offline.html",
+        offlineResources = { "./images/offline.png"}
+)
+@SpringBootApplication
+@Push
+@Theme(value = "chronos", variant = Lumo.DARK)
+public class FrontendApplication implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.addFavIcon("icon", "icons/favicon.ico", "16x16");
+        settings.addLink("shortcut icon", "icons/favicon.ico");
+    }
+}
