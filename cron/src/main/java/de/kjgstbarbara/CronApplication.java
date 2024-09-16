2af4fdcfbdee3dbf638013078ab7ec20b6ef9d4f
justus <justushenze@gmx.de>
2024-09-16 09:06:39 +0200

    [TASK] Rename Cron Main Class to CronApplication

--- cron/src/main/java/de/kjgstbarbara/CronApplication.java
+++ cron/src/main/java/de/kjgstbarbara/CronApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class CronApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(CronApplication.class, args);
     }
 }
