e1142c52a41fce666e06d732188e0671ee4b7b11
justus <henze5@gmx.de>
2024-10-01 22:17:55 +0200

    [TASK] Remove GameEvaluation Module

--- frontend/src/main/java/de/kjgstbarbara/services/ParticipantRepository.java
+++
@@ -1,8 +0,0 @@
-package de.kjgstbarbara.services;
-
-import de.kjgstbarbara.data.Participant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface ParticipantRepository extends JpaRepository<Participant, Long>, JpaSpecificationExecutor<Participant> {
-}
--- frontend/src/main/java/de/kjgstbarbara/views/MainNavigationView.java
+++ frontend/src/main/java/de/kjgstbarbara/views/MainNavigationView.java
@@ -118,7 +118,6 @@ public class MainNavigationView extends AppLayout implements BeforeEnterObserver
         nav.addItem(new SideNavItem("Meine Termine", CalendarView.class, VaadinIcon.CALENDAR_USER.create()));
         nav.addItem(new SideNavItem("Gruppen", GroupView.class, VaadinIcon.GROUP.create()));
         nav.addItem(new SideNavItem("Organisationen", OrganisationView.class, VaadinIcon.OFFICE.create()));
-        nav.addItem(new SideNavItem("Spieleauswertung", GameEvaluationListView.class, VaadinIcon.GAMEPAD.create()));
 
         return nav;
     }
