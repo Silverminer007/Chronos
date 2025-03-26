4cfbbd456bb70c0f30807a4f5da2c87ca755cfa7
justus <henze5@gmx.de>
2025-03-26 18:56:39 +0100

    create ical endpoints

---
+++ src/main/java/de/chronoslive/controllers/ICalController.java
@@ -0,0 +1,84 @@
+package de.chronoslive.controllers;
+
+import biweekly.Biweekly;
+import biweekly.ICalendar;
+import de.chronoslive.entitys.Person;
+import de.chronoslive.services.ICalService;
+import de.chronoslive.services.PersonsService;
+import de.chronoslive.utility.FrontendUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping(path = "/api/v1/ical")
+public class ICalController {
+    private final ICalService icalService;
+    private final PersonsService personsService;
+
+    public ICalController(ICalService icalService, PersonsService personsService) {
+        this.icalService = icalService;
+        this.personsService = personsService;
+    }
+
+    @GetMapping(path = "/group/{groupID}.ics", produces = "text/calendar")
+    public ResponseEntity<String> getCalendarByGroupID(@PathVariable("groupID") long groupID) {
+        ICalendar calendar = this.icalService.getCalendarByGroup(groupID);
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Disposition", "filename=Kalender.ics")
+                .body(result);
+    }
+
+    @GetMapping(path = "/organisation/{organisationId}.ics", produces = "text/calendar")
+    public ResponseEntity<String> getCalendarByOrganisationID(@PathVariable("organisationId") long organisationID) {
+        ICalendar calendar = this.icalService.getCalendarByOrganisation(organisationID);
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Disposition", "filename=Kalender.ics")
+                .body(result);
+    }
+
+    @GetMapping(path = "/organisation/{organisationId}/public.ics", produces = "text/calendar")
+    public ResponseEntity<String> getPublicCalendarByOrganisationID(@PathVariable("organisationId") long organisationID) {
+        ICalendar calendar = this.icalService.getPublicCalendarByOrganisation(organisationID);
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Disposition", "filename=Kalender.ics")
+                .body(result);
+    }
+
+    @GetMapping(path = "/date/{dateId}.ics", produces = "text/calendar")
+    public ResponseEntity<String> getCalendarByDateID(@PathVariable("dateId") long dateId) {
+        ICalendar calendar = this.icalService.getCalendarByDate(dateId);
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Disposition", "filename=Kalender.ics")
+                .body(result);
+    }
+
+    @GetMapping(path = "/date/year/{year}.ics", produces = "text/calendar")
+    public ResponseEntity<String> getCalendarByYear(@PathVariable("year") int year, @RequestParam(name = "token") String token) {
+        ICalendar calendar = this.icalService.getCalendarByYear(year);
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Disposition", "filename=Kalender.ics")
+                .body(result);
+    }
+
+    @GetMapping(path = "/ical.ics", produces = "text/calendar")
+    public ResponseEntity<String> getCalendar() {
+        Person principal = FrontendUtils.getAuthenticatedUser(this.personsService.getPersonsRepository()).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+        ICalendar calendar = this.icalService.getCalendarByPrincipal(principal);
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping(path = "/public.ics", produces = "text/calendar")
+    public ResponseEntity<String> getPublicCalendar() {
+        ICalendar calendar = this.icalService.getPublicCalendar();
+        String result = Biweekly.write(calendar).go();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
---
+++ src/main/java/de/chronoslive/services/ICalService.java
@@ -0,0 +1,147 @@
+package de.chronoslive.services;
+
+import biweekly.ICalendar;
+import biweekly.component.VEvent;
+import biweekly.property.Attendee;
+import de.chronoslive.entitys.Date;
+import de.chronoslive.entitys.Feedback;
+import de.chronoslive.entitys.Person;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ICalService {
+    private final DatesService datesService;
+
+    public ICalService(DatesService datesService) {
+        this.datesService = datesService;
+    }
+
+    public ICalendar getCalendarByGroup(long groupId) {
+        ICalendar calendar = new ICalendar();
+
+        List<Date> events = this.datesService.getDateRepository().findByGroupId(groupId);
+
+        for (Date event : events) {
+            VEvent vEvent = dateToVEvent(event);
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    public ICalendar getCalendarByOrganisation(long organisationId) {
+        ICalendar calendar = new ICalendar();
+
+        List<Date> events = this.datesService.getDateRepository().findByGroupOrganisationId(organisationId);
+
+        for (Date event : events) {
+            VEvent vEvent = dateToVEvent(event);
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    public ICalendar getCalendarByDate(long dateId) {
+        ICalendar calendar = new ICalendar();
+
+        Optional<Date> event = this.datesService.getDateRepository().findById(dateId);
+
+        if (event.isPresent()) {
+            VEvent vEvent = dateToVEvent(event.get());
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    public ICalendar getCalendarByYear(int year) {
+        ICalendar calendar = new ICalendar();
+
+        LocalDateTime yearStart = LocalDateTime.of(year, 1, 1, 0, 0);
+        long time = System.currentTimeMillis();
+        List<Date> events = this.datesService.getDateRepository().findByStartBeforeAndEndAfter(yearStart.withYear(year + 1), yearStart);
+        System.out.println(System.currentTimeMillis() - time);
+
+        for (Date event : events) {
+            VEvent vEvent = dateToVEvent(event);
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    public ICalendar getCalendarByPrincipal(Person person) {
+        ICalendar calendar = new ICalendar();
+
+        List<Date> events = this.datesService.getDateRepository().findByGroupMembersInAndGroupOrganisationMembersIn(List.of(person), List.of(person));
+
+        for (Date event : events) {
+            VEvent vEvent = dateToVEvent(event);
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    public ICalendar getPublicCalendar() {
+        ICalendar calendar = new ICalendar();
+
+        List<Date> events = this.datesService.getDateRepository().findByPublish(true);
+
+        for (Date event : events) {
+            VEvent vEvent = dateToVEvent(event);
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    public ICalendar getPublicCalendarByOrganisation(long organisationId) {
+        ICalendar calendar = new ICalendar();
+
+        List<Date> events = this.datesService.getDateRepository().findByPublishAndGroupOrganisationId(true, organisationId);
+
+        for (Date event : events) {
+            VEvent vEvent = dateToVEvent(event);
+
+            calendar.addEvent(vEvent);
+        }
+
+        return calendar;
+    }
+
+    private VEvent dateToVEvent(Date event) {
+        VEvent vEvent = new VEvent();
+        vEvent.setUid(String.valueOf(event.getId()));
+
+        vEvent.setSummary(event.getTitle());
+
+        vEvent.setDateStart(java.util.Date.from(event.getStart().atZone(ZoneOffset.UTC).toInstant()));
+        vEvent.setDateEnd(java.util.Date.from(event.getEnd().atZone(ZoneOffset.UTC).toInstant()));
+
+        vEvent.addRelatedTo(String.valueOf(event.getLinkedTo()));
+
+        vEvent.setDescription(event.getNotes());
+
+        vEvent.setLocation(event.getVenue());
+
+        for (Person member : event.getGroup().getMembers()) {
+            if (event.getStatusFor(member).equals(Feedback.Status.COMMITTED)) {
+                vEvent.addAttendee(new Attendee(member.getName(), member.getEMailAddress()));
+            }
+        }
+        return vEvent;
+    }
+}
